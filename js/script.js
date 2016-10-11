7088647bef6612591863439eff2c9ab28916cbb1
WildBacon <romansidorenko1990@gmail.com>
2016-10-11 14:33:29 +0300

    Добавлена интерактивная карта Google, скрипты вынесены в отдельный файл

---
+++ js/script.js
@@ -0,0 +1,66 @@
+
+      var link = document.querySelector(".login");
+      var popup = document.querySelector(".modal-content");
+      var close = document.querySelector(".modal-content-close");
+      var login = popup.querySelector("[name=login]");
+      var form = popup.querySelector("form");
+      var password = popup.querySelector("[name=password]");
+      var storage = localStorage.getItem("login");
+      
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.toggle("modal-content-show");
+        if (storage) {
+          login.value = storage;
+          password.focus();
+        } else {
+          login.focus();
+        }
+      });
+      
+      close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("modal-content-show");
+      });
+      
+      
+      form.addEventListener("submit", function(event) {
+        if (!(login.value && password.value)) {
+          event.preventDefault();
+          console.log("User didn't fill fields");
+          popup.classList.add("modal-error");
+        } else {
+          localStorage.setItem("login", login.value);
+        }
+      });
+      
+      window.addEventListener("keydown", function(event) {
+        if (event.keyCode == 27) {
+          if (popup.classList.contains("modal-content-show")) {
+            popup.classList.remove("modal-content-show");
+          }
+        }
+      });
+      
+      var mappopup = document.querySelector(".map-block");
+      var maplink = document.querySelector(".how-to-go");
+      var mapclose = mappopup.querySelector(".map-close");
+      
+      
+      maplink.addEventListener("click", function(event) {
+        event.preventDefault();
+        mappopup.classList.toggle("map-block-show");
+      });
+      
+      mapclose.addEventListener("click", function(event) {
+        event.preventDefault();
+        mappopup.classList.remove("map-block-show");
+      });
+      
+      window.addEventListener("keydown", function(event) {
+        if (event.keyCode == 27) {
+          if (mappopup.classList.contains("map-block-show")) {
+            mappopup.classList.remove("map-block-show");
+          }
+        }
+      });
